535d9e69ea9a4e0b2ec744aed8aaee774596d9e9
Daniel Klava <daniel.klava@br.ey.com>
2024-07-15 22:56:51 -0300

    Retirado WIP de validação de data (já implementado)

--- src/constantes/uiConstantes.ts
+++ src/constantes/uiConstantes.ts
@@ -3,7 +3,6 @@ export const labels: { [key: string]: any } = {
         nome : "Informe o nome do role",
         local : "Informe o local que ocorrerá o role, para auxiliar as pessoas que vão a encontrar o local, informe tambem o endereço e demais informações referente a localização.",
         data : "Informe a data do role no formato: DD/MM/AAAA ex 31/12/2024",
-        dataErro : "Data inserida no formato inválido.\nInforme a data do role no formato: DD/MM/AAAA ex 31/12/2024.",
         hora : "Informe a hora que começará o role, ou a hora de encontro",
         descricao : "Informe uma descrição para que todos possam saber sobre o que se trata, forneça informações importantes sobre o lugar e/ou atividades a serem realizadas",
         resumo : `-----*ROLE*-----\n*$role.nome*\n\n*Local*: $role.local\n\n*Data*: $role.data *Hora:* $role.hora\n\n$role.descricao\n\n*CONFIRMADOS ($role.participantes)*\n\n---------------------------$participantes\n\n---------------------------\n\nPara *confirmar* digite */confirmar $role_sequencial*\n\nPara *desconfirmar* basta digite */miar $role.sequencial*`,
