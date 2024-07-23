20033556aedd2e176418a9be7353a430b0034a8d
Daniel Klava <daniel.klava@br.ey.com>
2024-07-23 19:56:18 -0300

    Revert chromium args

--- src/client/client.ts
+++ src/client/client.ts
@@ -12,14 +12,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true , args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
+            '--no-sandbox', '--disable-setuid-sandbox'
         ],
       },
       webVersionCache: {
